998539a8d5bf3c99900d93928abd533c1f273cc2
Barry Schwartz <github@crudfactory.com>
2013-07-29 18:37:44 -0500

    Automagical ChangeLog generation, using Gnulib.

--- autogen.sh
+++ autogen.sh
@@ -9,11 +9,20 @@ test -n "$srcdir" || srcdir=.
 olddir=`pwd`
 cd $srcdir
 
+echo -n "checking for gnulib-tool... "
+which gnulib-tool || {
+	echo "*** No gnulib-tool found, please install it ***"
+	exit 1
+}
+
 echo -n "checking for autoreconf... "
 which autoreconf || {
 	echo "*** No autoreconf found, please install it ***"
 	exit 1
 }
+
+echo "running gnulib-tool --update"
+gnulib-tool --update || exit $?
 
 echo "running autoreconf --install --verbose"
 autoreconf --install --verbose || exit $?
